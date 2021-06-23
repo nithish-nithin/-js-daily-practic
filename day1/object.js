e6e8b1a402f6c743ea7aac7c969c3617e3d19dc4
nithish_nthin <techk4240@gmail.com>
2021-06-23 01:26:52 -0700

    daily  practic of js

---
+++ day1/object.js
@@ -0,0 +1,50 @@
+// this a practic of  js object 
+
+let person1={
+    name:'nithish',
+    nickName:'nithin',
+    age:19,
+    address: "chennai, tamilnadu",
+    siblind:{
+        brother:'tamizarsu',
+        sister: 'preethi',
+    }
+
+}
+
+let person2={
+    name:'tamizharasu',
+    nickName:'tamizh',
+    age:20,
+    address: "chennai, tamilnadu",
+    siblings:{
+        brother:'nithish',
+        sister: 'renna rai',
+    }
+
+}
+
+
+
+  
+  
+
+let person3 ={
+    name:'reena rai',
+    nickName:'renna',
+    age:22,
+    address: "chennai, tamilnadu",
+    siblind:{
+        brother:'tamizarsu',
+        sister: 'preethi',
+    }
+
+}
+ 
+ console.log(person1);
+ console.log(person2);
+ console.log(person3);
+
+
+
+
